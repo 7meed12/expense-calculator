bed9d0d0096e4a810c9b78224fa33bf278208244
abdulhameedkhaled <abdulhameed.khaled12@gmail.com>
2021-10-20 00:50:27 +0200

    first component commited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-
-import './App.css';
+import ExpenseItem from "./Components/ExpenseItem";
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
   return (
-    <div><p>hello react</p></div>
+    <ExpenseItem 
+    title={expenses[0].title }
+    amount={expenses[0].amount}
+    date={expenses[0].date}
+    ></ExpenseItem>
   );
 }
 
